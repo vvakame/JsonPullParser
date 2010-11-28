965e9ec51d1383277770a500fa50a14e94e4cf80
vvakame <vvakame+dev@gmail.com>
2010-11-28 17:55:45 +0900

    性能比較用のテストを追加

---
+++ test/net/vvakame/util/jsonpullparser/JSONPullParserSpecTest.java
@@ -0,0 +1,52 @@
+package net.vvakame.util.jsonpullparser;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import net.arnx.jsonic.JSON;
+import net.vvakame.util.jsonpullparser.JSONPullParser.Current;
+
+import org.junit.Test;
+
+public class JSONPullParserSpecTest {
+
+	@Test
+	public void specMyLib() throws IOException, JSONFormatException {
+		String json = getJson();
+		System.gc();
+		long start = System.currentTimeMillis();
+		JSONPullParser parser = new JSONPullParser();
+		ByteArrayInputStream stream = new ByteArrayInputStream(json.getBytes());
+		parser.setInput(stream);
+		Current current = null;
+		while (current != Current.END_ARRAY) {
+			current = parser.getEventType();
+		}
+		long end = System.currentTimeMillis();
+
+		System.out.println(end - start);
+	}
+
+	@Test
+	public void specJSONIC() throws IOException, JSONFormatException {
+		String json = getJson();
+		System.gc();
+		long start = System.currentTimeMillis();
+		@SuppressWarnings("unused")
+		Object decode = JSON.decode(json);
+		long end = System.currentTimeMillis();
+
+		System.out.println(end - start);
+	}
+
+	public String getJson() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[");
+		for (int i = 0; i < 1000000; i++) {
+			builder.append(i).append(",");
+		}
+		builder.setLength(builder.length() - 1);
+		builder.append("]");
+		return builder.toString();
+	}
+}

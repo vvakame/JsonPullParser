3d3da724f67b54430d3f5716b7de5814f7fa33c0
vvakame <vvakame+dev@gmail.com>
2010-12-29 13:11:52 +0900

    不要になっていたファイルの削除

--- jsonpullparser-apt/src/test/java/net/vvakame/sample/twitter/Attribute.java
+++
@@ -1,7 +0,0 @@
-package net.vvakame.sample.twitter;
-
-import net.vvakame.util.jsonpullparser.annotation.JsonModel;
-
-@JsonModel
-public class Attribute {
-}
--- jsonpullparser-core/src/main/java/net/vvakame/util/jsonpullparser/annotation/OnJsonObjectAddListener.java
+++
@@ -1,5 +0,0 @@
-package net.vvakame.util.jsonpullparser.annotation;
-
-public interface OnJsonObjectAddListener {
-	public void onAdd(Object obj);
-}

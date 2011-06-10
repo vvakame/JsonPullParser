10830ee598048680fbd9f78929d9f8f8547c1bb7
vvakame <vvakame+dev@gmail.com>
2011-06-11 00:06:13 +0900

    SaveOriginal使用のテストを追加 bug fixなど

---
+++ jsonpullparser-usage/src/main/java/net/vvakame/sample/SaveOriginalData2.java
@@ -0,0 +1,71 @@
+package net.vvakame.sample;
+
+import net.vvakame.util.jsonpullparser.annotation.JsonKey;
+import net.vvakame.util.jsonpullparser.annotation.JsonModel;
+import net.vvakame.util.jsonpullparser.annotation.SaveOriginal;
+
+/**
+ * {@link SaveOriginal} 確認用クラス.
+ * @author vvakame
+ */
+@JsonModel(treatUnknownKeyAsError = false)
+public class SaveOriginalData2 {
+
+	@JsonKey
+	String str;
+
+	@JsonKey
+	long num;
+
+	@SaveOriginal(treatLogDisabledAsError = false)
+	String original;
+
+
+	/**
+	 * @return the str
+	 * @category accessor
+	 */
+	public String getStr() {
+		return str;
+	}
+
+	/**
+	 * @param str the str to set
+	 * @category accessor
+	 */
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+	/**
+	 * @return the num
+	 * @category accessor
+	 */
+	public long getNum() {
+		return num;
+	}
+
+	/**
+	 * @param num the num to set
+	 * @category accessor
+	 */
+	public void setNum(long num) {
+		this.num = num;
+	}
+
+	/**
+	 * @return the original
+	 * @category accessor
+	 */
+	public String getOriginal() {
+		return original;
+	}
+
+	/**
+	 * @param original the original to set
+	 * @category accessor
+	 */
+	public void setOriginal(String original) {
+		this.original = original;
+	}
+}
---
+++ jsonpullparser-usage/src/test/java/net/vvakame/util/jsonpullparser/annotation/SaveOriginalTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2011 vvakame <vvakame@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.vvakame.util.jsonpullparser.annotation;
+
+import java.io.IOException;
+
+import net.vvakame.sample.SaveOriginalData1;
+import net.vvakame.sample.SaveOriginalData1Generated;
+import net.vvakame.sample.SaveOriginalData2;
+import net.vvakame.sample.SaveOriginalData2Generated;
+import net.vvakame.util.jsonpullparser.JsonFormatException;
+import net.vvakame.util.jsonpullparser.JsonPullParser;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * {@link SaveOriginal} にパラメータを与えた場合のテスト.
+ * @author vvakame
+ */
+public class SaveOriginalTest {
+
+	/**
+	 * {@link JsonPullParser#setLogEnable()} が呼ばれていない場合に例外を飛ばす.
+	 * @author vvakame
+	 * @throws JsonFormatException 
+	 * @throws IOException 
+	 */
+	@Test(expected = IllegalStateException.class)
+	public void treatError() throws IOException, JsonFormatException {
+		String json = "{\"str\":\"hoge\",\"num\":1}";
+		JsonPullParser parser = JsonPullParser.newParser(json);
+		SaveOriginalData1Generated.get(parser);
+	}
+
+	/**
+	 * {@link JsonPullParser#setLogEnable()} が呼ばれていなくても例外を飛ばさない.
+	 * @author vvakame
+	 * @throws JsonFormatException 
+	 * @throws IOException 
+	 */
+	@Test
+	public void ignoreError() throws IOException, JsonFormatException {
+		String json = "{\"str\":\"hoge\",\"num\":1}";
+		JsonPullParser parser = JsonPullParser.newParser(json);
+		SaveOriginalData2 data = SaveOriginalData2Generated.get(parser);
+
+		assertThat(data.getStr(), is("hoge"));
+		assertThat(data.getNum(), is(1L));
+		assertThat(data.getOriginal(), nullValue());
+	}
+
+	/**
+	 * {@link JsonPullParser#setLogEnable()} が呼ばれていない場合に例外を飛ばす.
+	 * @author vvakame
+	 * @throws JsonFormatException 
+	 * @throws IOException 
+	 */
+	@Test
+	public void saveOriginal1() throws IOException, JsonFormatException {
+		String json = "{\"str\":\"hoge\",\"num\":1    ,   \"test\":true}";
+		JsonPullParser parser = JsonPullParser.newParser(json).setLogEnable();
+		SaveOriginalData1 data = SaveOriginalData1Generated.get(parser);
+
+		assertThat(data.getOriginal(), is("{\"str\":\"hoge\",\"num\":1,\"test\":true}"));
+	}
+
+	/**
+	 * {@link JsonPullParser#setLogEnable()} が呼ばれていない場合に例外を飛ばす.
+	 * @author vvakame
+	 * @throws JsonFormatException 
+	 * @throws IOException 
+	 */
+	@Test
+	public void saveOriginal2() throws IOException, JsonFormatException {
+		String json =
+				"{\"str\":\"hoge\",\"num\":1    ,   \"test\":true, \"ary\":[1,2,3,4,5,6], \t \r\n \"hash\":{\"╮( ╹ω╹ )╭\": false}}";
+		JsonPullParser parser = JsonPullParser.newParser(json).setLogEnable();
+		SaveOriginalData1 data = SaveOriginalData1Generated.get(parser);
+
+		assertThat(
+				data.getOriginal(),
+				is("{\"str\":\"hoge\",\"num\":1,\"test\":true,\"ary\":[1,2,3,4,5,6],\"hash\":{\"╮( ╹ω╹ )╭\":false}}"));
+	}
+}
